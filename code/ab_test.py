7c7de845829b075d6dc9cf049b31f072515b4fb8
ubin <ub.devl0908@gmail.com>
2026-04-21 12:41:42 +0900

    feat: add A/B test for CTR

---
+++ code/ab_test.py
@@ -0,0 +1,36 @@
+import math
+from scipy import stats
+
+a_imps = int(input("a 노출수 : "))
+a_clicks = int(input("a 클릭수 : "))
+b_imps = int(input("b 노출수 : "))
+b_clicks = int(input("b 클릭수 : "))
+
+# 클릭률
+a_ctr = float(a_clicks / a_imps)
+b_ctr = float(b_clicks / b_imps)
+
+print(a_ctr, b_ctr)
+
+# 클릭률 차이
+ctr_diff = abs(b_ctr - a_ctr)
+
+# 클릭률 표준오차
+a_SE = math.sqrt((a_ctr*(1-a_ctr))/a_imps)
+b_SE = math.sqrt((b_ctr*(1-b_ctr))/b_imps)
+
+SE_diff = round(math.sqrt(a_SE**2 + b_SE**2), 5)
+
+#z-score
+z = round(ctr_diff/SE_diff, 2)
+print('z :', z )
+
+p_value = 2 * (1 - stats.norm.cdf(abs(z))) # 양측 검증이라 1에서 뺌
+print(p_value)
+
+if p_value >= 0.05:
+    print('귀무가설 기각 실패, p-value : ', p_value)
+elif p_value < 0.05:
+    print('귀무가설 기각, p-value : ', p_value)
+else:
+    print('error')
